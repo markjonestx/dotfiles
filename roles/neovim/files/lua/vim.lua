b231164f794aa164f0f45f05b489f691188df771
Mark Jones <markjonestx@gmail.com>
2023-07-10 02:09:22 -0400

    Add autogroups and colorcolumn
    I know the community has strong opinions about colorcolumn, but I live
    by it and they can pry it out of my cold dead hands.
    
    Also adding the beginning of autotyping to adjust settings for
    particular file types. To begin with, Puppet uses (or at least at JLab)
    uses 2 spaces for indentation, but my default is 4- so now I can open
    Puppet files and I don't need to adjust settings. Next, I adjusted the
    colorcolumn for Rust, since Rust's spec actually states the longest a
    line can be is 99 characters.

--- roles/neovim/files/lua/vim.lua
+++ roles/neovim/files/lua/vim.lua
@@ -6,4 +6,19 @@ set.shiftwidth = 4
 set.expandtab = true
 set.number = true
 set.relativenumber = true
+set.colorcolumn = "79"
+
+vim.cmd([[
+    augroup rustColorColumn
+        autocmd!
+        autocmd FileType rust setlocal colorcolumn=99
+    augroup END
+]])
+
+vim.cmd([[
+    augroup puppetIndent
+        autocmd!
+        autocmd FileType puppet setlocal shiftwidth=2
+    augroup END
+]])
 
