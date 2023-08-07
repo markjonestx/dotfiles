8d57cbd8eafc41bc60fc6c1fd1e9b400c953b397
Mark Jones <markjonestx@gmail.com>
2023-08-07 01:50:04 -0400

    wezterm: Fix Comic Code Font
    The configuration was set incorrectly for WezTerm, which was resulting
    in it setting the font to Jetbrain's Mono font.

--- roles/wezterm/files/wezterm.lua
+++ roles/wezterm/files/wezterm.lua
@@ -2,16 +2,13 @@ local wezterm = require 'wezterm'
 
 local config = {
     window_background_opacity = 0.86,
---    color_scheme = 'Tokyo Night',
     color_scheme = 'Catppuccin Mocha',
 
     hide_tab_bar_if_only_one_tab = true,
     tab_bar_at_bottom = false,
     use_fancy_tab_bar = false,
-}
-
-config.window_frame = {
-    font = wezterm.font { family = 'ComicCodeLigatures Nerd Font', },
+    font = wezterm.font('ComicCodeLigatures Nerd Font'),
+    font_size = 11
 }
 
 config.colors = {
@@ -19,4 +16,3 @@ config.colors = {
 }
 
 return config
-
