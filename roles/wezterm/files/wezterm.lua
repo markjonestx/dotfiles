e8d6040010cb0f500aac396bfbad2a094a0fa700
Mark Jones <maj@jlab.org>
2023-08-07 11:03:19 -0400

    wez: Clean configuration and increase tansparency

--- roles/wezterm/files/wezterm.lua
+++ roles/wezterm/files/wezterm.lua
@@ -1,12 +1,13 @@
 local wezterm = require 'wezterm'
 
 local config = {
-    window_background_opacity = 0.86,
+    window_background_opacity = 0.82,
     color_scheme = 'Catppuccin Mocha',
 
     hide_tab_bar_if_only_one_tab = true,
     tab_bar_at_bottom = false,
     use_fancy_tab_bar = false,
+
     font = wezterm.font('ComicCodeLigatures Nerd Font'),
     font_size = 11
 }
