7b784b8e5eebc711a0778360bfabed184a1d328f
Mark Jones <maj@jlab.org>
2024-03-26 08:41:41 -0400

    Limit the bar to only be on the main display
    I need to roll out templates for this, because the "chin" on the top of
    the primary display is a different thickness per- Mac Laptop
    
    Maybe there's a way to ask the system itself?

--- roles/sketchybar/files/sketchy/bar.lua
+++ roles/sketchybar/files/sketchy/bar.lua
@@ -3,8 +3,9 @@ local colors = require("colors")
 -- Equivalent to the --bar domain
 sbar.bar({
   topmost = "window",
-  height = 40,
+  height = 38,
   color = colors.bar.bg,
   padding_right = 2,
   padding_left = 2,
+  display = 1
 })
