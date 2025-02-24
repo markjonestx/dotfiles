b4ce95e68d0db5da2a57eff81a345d9039e1e304
Mark Jones <maj@jlab.org>
2025-02-24 09:10:40 -0500

    Disable Copilot load by default

--- roles/neovim/files/lua/plugins/code.lua
+++ roles/neovim/files/lua/plugins/code.lua
@@ -45,7 +45,7 @@ return {
 
             local has_words_before = function()
               if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
-              local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+              local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
               return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
             end
 
@@ -223,7 +223,7 @@ return {
     -- Github Copilot, for intelligent autofill- where it makes sense
     {
         'zbirenbaum/copilot.lua',
-        lazy = false,
+        cmd = { 'Copilot' },
         opts = {
             suggestion = { enabled = false },
             panel = { enabled = false },
