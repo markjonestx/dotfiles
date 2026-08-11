a9bf9c1a8105af2ab821e715cdb9e17c6df1cc81
Mark Jones <markjonestx@live.com>
2026-08-10 20:39:38 -0400

    Add NIX flake environment

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Ansible Environment for Network deployment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            ansible
+            ansible-lint
+            python3
+            sshpass
+          ];
+
+          shellHook = ''
+            echo "Ansible environment is ready."
+            ansible --version
+          '';
+        };
+      }
+    );
+}
