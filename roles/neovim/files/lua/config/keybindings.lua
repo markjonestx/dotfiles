7ea1269de398abd1a5d05e4803b33f640a101b6a
Mark Jones <maj@jlab.org>
2026-09-24 02:35:24 -0400

    nvim: Remove \x and \d keymap since they conflict with trouble

--- roles/neovim/files/lua/config/keybindings.lua
+++ roles/neovim/files/lua/config/keybindings.lua
@@ -5,8 +5,6 @@ keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 keymap.set({ 'n', 'v' }, '<C-u>', '<C-u>zz')
 keymap.set({ 'n', 'v' }, '<C-d>', '<C-d>zz')
 keymap.set('x', 'p', '"_dP')
-keymap.set({ 'n', 'v' }, '<leader>x', '"_x')
-keymap.set({ 'n', 'v' }, '<leader>d', '"_d')
 keymap.set('n', '<C-w>v', '<C-w>v<C-w>l')
 keymap.set('n', '<C-w>s', '<C-w>s<C-w>j')
 
